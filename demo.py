0249190b05c66e3d5bcb57782ed528bde485be83
faizwhb <fwahhab89@gmail.com>
2022-09-14 20:03:50 +0200

    update demo to save image files as well

--- demo.py
+++ demo.py
@@ -34,8 +34,8 @@ def main(args):
         print(os.path.join(args.results_dir, file_name + ".png"))
         cv2.imwrite(os.path.join(args.results_dir, file_name + ".png"), canvas)
         final_result = candidate[0]
-        with open(os.path.join(args.results_dir, file_name + ".json"), "w") as out_file:
-            json.dump({"result": final_result}, out_file)
+        #with open(os.path.join(args.results_dir, file_name + ".json"), "w") as out_file:
+        #    json.dump({"result": final_result}, out_file)
 
 
 if __name__ == '__main__':
